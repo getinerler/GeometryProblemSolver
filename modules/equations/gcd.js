371ebc72a9112d6d9d2d7dc5caca9fa9fec6917e
Güven Çetinerler <getinerler@gmail.com>
2024-03-06 17:17:41 +0300

    Fix bug: Correct GCD calculation for numbers 2 and 3

--- modules/equations/gcd.js
+++ modules/equations/gcd.js
@@ -1,23 +1,25 @@
 'use strict';
 
-export function getGcd(val1, val2) {
-    if (val1 === 0 || val2 === 0 || val1 === 1 || val2 === 1) {
+export function getGcd(num1, num2) {
+    if (num1 === 0 || num2 === 0 || num1 === 1 || num2 === 1) {
         return 1;
     }
-    if (!val1 || !val2) {
-        throw (`getGcd: Wrong parameters (${val1}, ${val2}).`);
+    if (!num1 || !num2) {
+        throw (`getGcd: Wrong parameters (${num1}, ${num2}).`);
     }
-    let big = val1 > val2 ? val1 : val2;
-    let small = val1 < val2 ? val1 : val2;
+
+    let big = num1 > num2 ? num1 : num2;
+    let small = num1 < num2 ? num1 : num2;
+
     let counter = 0;
-    while (small !== 0 && small !== 1 && big !== NaN && small !== NaN) {
-        if (counter > 1000) {
+    while (small !== 0 && big !== NaN && small !== NaN) {
+        if (counter++ > 1000) {
             throw (`getGcd: More than 1000 trials (${big}, ${small}).`);
         }
         let temp = big % small;
         big = small;
         small = temp;
-        counter++;
     }
+    
     return Math.abs(big);
 }
